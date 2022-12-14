4c34ec9a9154c1359bdaca14c35b9860201a6e25
huntinator7 <huntinator7@gmail.com>
2022-12-14 01:14:44 -0700

    removed vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,9 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { VueFire, VueFireAuth } from "vuefire";
 import VueSidebarMenu from "vue-sidebar-menu";
-import { createVuetify } from "vuetify";
-import { VBtn } from "vuetify/components";
-import * as directives from "vuetify/directives";
 
 import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-import "vuetify/styles";
 
 import App from "./App.vue";
 import router from "./router";
@@ -16,21 +12,12 @@ import { firebaseApp } from "./firebase";
 
 import "./assets/main.css";
 
-const components = { VBtn };
-console.log(components);
-
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-
 const app = createApp(App);
 
 // app.component("font-awesome-icon", FontAwesomeIcon);
 app.use(createPinia());
 app.use(router);
 app.use(VueSidebarMenu);
-app.use(vuetify);
 
 app.use(VueFire, {
   firebaseApp,
