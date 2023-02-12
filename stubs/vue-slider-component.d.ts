ca5a091d01e75880a294291a77b1c3f99e6ce4a3
huntinator7 <huntinator7@gmail.com>
2023-02-11 21:24:19 -0700

    stupid slider

---
+++ stubs/vue-slider-component.d.ts
@@ -0,0 +1,5 @@
+// stub/vue-slider-component.d.ts
+// This needs to be stubbed until the vue-slider-component library fix their types
+// https://github.com/johnsoncodehk/volar/issues/2018
+
+export { DefineComponent } from "vue";
