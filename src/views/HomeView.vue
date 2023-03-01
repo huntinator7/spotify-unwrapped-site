694ddf1323a98e8516ede2febdf92150d50b6dba
huntinator7 <huntinator7@gmail.com>
2023-02-28 23:58:27 -0700

    Unwrapped February

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <h1>Welcome to Unwrapped!</h1>
   <main class="content">
     <span class="large"
-      >Unwrapped January is here! Make sure to go
+      >Unwrapped February is here! Make sure to go
       <router-link to="/unwrapped">check it out!</router-link></span
     >
     <Login />
