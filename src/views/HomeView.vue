b7711873ec0cec87d92d03f951fddebbe4dfe388
huntinator7 <huntinator7@gmail.com>
2023-11-06 15:12:48 -0700

    octobro

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <h1>Welcome to Unwrapped!</h1>
   <main class="content">
     <span class="large"
-      >Unwrapped July is here! Make sure to go
+      >Unwrapped October is here! Make sure to go
       <router-link to="/unwrapped">check it out!</router-link></span
     >
     <Login />
